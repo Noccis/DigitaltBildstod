c19cd32eaac956fc707c3cbe05f8ec35df62b5c4
Noccis <89582846+Noccis@users.noreply.github.com>
2021-12-28 11:08:35 +0100

    first commit

---
+++ app/src/main/java/com/example/digitaltbildstd/Action.kt
@@ -0,0 +1,26 @@
+package com.example.digitaltbildstd
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "action_table")
+data class Action(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "image") var image: Int,
+    @ColumnInfo(name = "image_text") var imageText: String,
+    @ColumnInfo(name = "done") var done: Boolean,
+    @ColumnInfo(name = "order") var order: Int,
+    @ColumnInfo(name = "steps") var steps: Boolean
+) {
+    // val listOfSteps: MutableList<Action>
+}
+
+/*
+
+@Entity(tableName = "word_table")
+data class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "english") var english : String,
+    @ColumnInfo(name= "swedish") var swedish: String)
+ */
